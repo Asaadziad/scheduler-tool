4a65abf5827193ac7fc182b0a5e07f4c22cbfaca
Asaadziad <asaadabuelhija@gmail.com>
2023-12-05 06:01:12 +0200

    gsh

---
+++ task.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+class task:
+    def __init__(self,name : str ,isFinished : bool) -> None:
+        self.name = name
+        self.isFinished = isFinished
+        self.date = date.today()
+    
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Date: {self.date}, Done: [ {self.isFinished} ]"
+    
+    def setName(self,new_name: str) -> None:
+        self.name = new_name
+    def setIsFinished(self, isFinished : bool) -> None:
+        self.isFinished = isFinished
+
+def editTask(old_task: task, new_name: str, task_finished: bool) -> None:
+    old_task.setName(new_name)
+    old_task.setIsFinished(task_finished)
+
+def addNewTask(tasks: list, new_task : task) -> None:
+    tasks.append(new_task)
+
+def displayTasks(tasks: list) -> None:
+    x = 0
+    while(x < len(tasks)):
+        print(tasks[x])
+        x = x+1
+
